4e9e472ed2f890d395d2476c1c333bc988768a3e
bzone <yarnbCoder@gmail.com>
2018-04-12 12:48:29 +0800

    fix: 新增发布脚本；兼容小程序和本地测试

--- src/global.js
+++ src/global.js
@@ -3,13 +3,5 @@
  * 在小程序框架 wepy 中，暂时无法解决循环加载的问题
  * 用一个全局存在的对象来挂载数据
  */
-const judgeEnv = env => {
-  switch (env) {
-    case 'wxapp':
-      return wx;
-    default:
-      return {};
-  }
-};
 
-export const globalObject = judgeEnv(process.env.PLAT_ENV);
+export const globalObject = {};
